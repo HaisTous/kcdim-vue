4e24b0b5f9cf4286e3f673aa3b8dac67a19bc635
HaisTous <vlad102013@mail.ru>
2023-03-08 19:51:04 +0300

    Удалены лишние файлы.

--- src/main.js
+++ src/main.js
@@ -1,14 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import components from "@/components/ui";
 import './assets/main.css'
 import router from "@/router/router";
 
 const app = createApp(App);
-
-components.forEach(component => {
-    app.component(component.name, component)
-})
 
 app
     .use(router)
